5fa7b220f9cc5c9afcb22ba466ff2ca2084be36b
nayayaaa <122560595+nayayaaa@users.noreply.github.com>
2023-01-13 13:13:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallHeart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+})
+basic.showIcon(IconNames.Heart)

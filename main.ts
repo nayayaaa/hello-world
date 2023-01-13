5a92eee6bc2019c72c3ac7d1197808881e98d8db
nayayaaa <122560595+nayayaaa@users.noreply.github.com>
2023-01-13 13:29:44 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.SmallHeart)
+    music.playMelody("B A G A G F A C5 ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
+    music.playMelody("G F G A - F E D ", 120)
 })
 basic.showIcon(IconNames.Happy)
